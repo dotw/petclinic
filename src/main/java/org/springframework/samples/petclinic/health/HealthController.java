0e0eefbd3d6006d920c61da9413abface3e3bfd0
HaoZong QI <qihaoz@cn.ibm.com>
2017-12-20 14:10:15 +0800

    health controller

---
+++ src/main/java/org/springframework/samples/petclinic/health/HealthController.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.health;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class HealthController {
+
+    @GetMapping("/health")
+    public String showVetList() {
+        return "welcome";
+    }
+}
